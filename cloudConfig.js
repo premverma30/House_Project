49979e6250cf91ddbfee4c87536867f80166c363
Prem Kumar Verma <premv9546@gmail.com>
2025-05-01 23:27:50 +0530

    Fix Cloudinary config typo

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,11 +1,18 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDE_NAME,
+//     api_key: process.env.CLOUDE_API_KEY,
+//     api_secret: process.env.CLOUDE_API_SECRET
+// })
+
 cloudinary.config({
-    cloud_name: process.env.CLOUDE_NAME,
-    api_key: process.env.CLOUDE_API_KEY,
-    api_secret: process.env.CLOUDE_API_SECRET
-})
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+});
+
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
